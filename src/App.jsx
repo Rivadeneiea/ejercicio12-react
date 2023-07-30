2d93246e4ee8ee1935f7e98f3d5778d4e721bec5
Rivadeneiea <joserivadeneira538@gmail.com>
2023-07-30 20:59:15 -0300

    creo funcion para consumir api

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    consultarApi();
+  }, []);
+
+  const consultarApi = async () => {
+    try {
+      const respuesta = await fetch(
+        `https://newsdata.io/api/1/news?apikey=pub_26815e3379be8009c57f199f4a5743ab2d2bb&category=${categoria}`
+      );
+      const dato = await respuesta.json();
+      setNoticias(dato.results);
+      console.log(dato);
+    } catch (error) {}
+  };
+
+  const selector = (e) => {
+    setCategoria(e.target.value);
+  };
+
   return (
     <>
       <h1>hola mundo</h1>
